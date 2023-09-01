64874ffe15436050aef0e1c77ba1a3504c2f733e
Serhii Demchenko <svdemchenko@gmail.com>
2023-09-01 21:57:13 +0300

    Add component and context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartItemShown, setCartItemShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartItemShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartItemShown(false);
+  };
+
   return (
-    <div>
-      <Header />
-    </div>
+    <CartProvider>
+      {cartItemShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
